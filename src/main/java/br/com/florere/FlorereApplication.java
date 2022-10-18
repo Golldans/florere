3c6fc24e9bd6f4ef95a084fe3e1dd278be23a14a
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-10-17 22:02:03 -0300

    initial commit

---
+++ src/main/java/br/com/florere/FlorereApplication.java
@@ -0,0 +1,13 @@
+package br.com.florere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/florere/FlorereApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.florere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
